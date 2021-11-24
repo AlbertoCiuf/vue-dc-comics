7fc1eac40c69975722829c7b39e5329e3954bbbb
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2021-11-24 16:36:46 +0100

    beta header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
-  <h1>aaa</h1>
+  <Header />
 </template>
 
 <script>
 
+import Header from "./components/Header.vue"
 
 export default {
   name: 'App',
   components: {
-
-  }
+    Header
+  },
+ 
 }
 </script>
 
 <style lang="scss">
+  @import "./assets/style/generals.scss";
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,83 @@
+<template>
+  <header>
+    <div class="container">
+      <a href="#">
+        <img src="../assets/img/dc-logo.png" alt="logo">
+      </a>
+      <nav>
+        <ul>
+          <li><a href="#">characters</a></li>
+          <li class="active"><a href="#">comics</a></li>
+          <li><a href="#">movies</a></li>
+          <li><a href="#">tv</a></li>
+          <li><a href="#">games</a></li>
+          <li><a href="#">collectibles</a></li>
+          <li><a href="#">videos</a></li>
+          <li><a href="#">fans</a></li>
+          <li><a href="#">news</a></li>
+          <li><a href="#">shop</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+   data(){
+    return{
+      topNavLinks: [
+        {
+          
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  header {
+    height: 120px;
+    // background-color: teal;
+    
+    .container {
+      height: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      img {
+        width: 85%;
+      }
+    }
+
+    ul {
+      display: flex;
+      li {
+        padding: 0 15px;
+        a {
+          line-height: 100%;
+          text-transform: uppercase;
+          text-decoration: none;
+          color: black;
+          font-weight: 700;
+       }
+          
+            
+          
+      li:hover a, li.active a {
+        color: #2582FA;
+        border-bottom: 2px solid #2582FA;
+
+       }
+    }
+
+  }
+ }
+
+</style>
+
+
+
